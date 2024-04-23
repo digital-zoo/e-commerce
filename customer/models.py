7c4a2567941fce32b651b6fc28d0cc6847fc308b
iseunglee <iseunglee99@gmail.com>
2024-04-23 16:43:26 +0900

    변경사항 적용

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Cart(models.Model):
+    cart_id = models.AutoField(primary_key=True)  # 카트ID는 자동으로 생성되는 순차적인 정수
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)  # Customer 테이블과 외래키 관계, 
+
+class CartItem(models.Model):
+    cartitem_id = models.AutoField(primary_key=True)  # 카트아이템ID는 자동으로 생성되는 순차적인 정수
+    cart_id = models.ForeignKey(Cart, on_delete=models.CASCADE)  # 카트와의 외래키 관계, 카트가 삭제되면 관련된 카트아이템도 삭제
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)  # product와 외래키 관계, 
+    quantity = models.IntegerField()  # 수량은 정수형
+    
