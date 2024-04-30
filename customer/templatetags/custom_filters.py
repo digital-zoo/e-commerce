2e2eace71aa731b3f64e4813141e2caf38d72c6c
Yeonhui-ili <yeonhui@ewhain.net>
2024-04-30 16:33:36 +0900

    feat:가격 표시 수정, 재고 내 수량 선택 추가. 페이지 연결은 안 된 상태

--- customer/templatetags/custom_filters.py
+++ customer/templatetags/custom_filters.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter
 def calc_discount(price, discount_rate):
     return price * (1 - discount_rate)
+
+@register.filter
+def multiply_by_100(value):
+    return value * 100
