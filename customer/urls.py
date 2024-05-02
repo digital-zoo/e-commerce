82b74398a51c59a369b1502021513a20a4b3093a
goldangeee <goldangeee@proton.me>
2024-05-02 20:12:18 +0900

    구매자 로그인,로그아웃

--- customer/urls.py
+++ customer/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     #선택된 카테고리 리스트로 이동
     path('<int:category_id>', CategoryList.as_view(),name='category_list'),
     path('product_detail/<int:product_id>', product_detail, name='product_detail'),
-
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
