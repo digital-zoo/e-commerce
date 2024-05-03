cd4fb50b24007370c5c0afa1f9c066d705d4ec82
Yeonhui-ili <yeonhui@ewhain.net>
2024-05-03 20:18:22 +0900

    feat:구매하기 절차 중 order관련 테이블 연동함. 실제 카드 결제 절차는 미완성

--- customer/views.py
+++ customer/views.py
@@ -6,35 +6,10 @@ from django.db.models import Sum
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.shortcuts import redirect
-
-# # 테스트용 가상 로그인
-# def simulate_login(request):
-#     # 기존에 생성한 사용자 정보 
-#     username = 'kkk'
-#     password = '1'
-
-#     # 해당 사용자로 로그인 시도
-#     user = authenticate(username=username, password=password)
-#     if user is not None:
-#         # 로그인 성공 시 세션에 사용자 정보 저장
-#         login(request, user)
-#         # return render(request, 'customer/checkout.html', {'user' : user})
-#     else:
-#         # 로그인 실패 시 오류 메시지 추가
-#         print('로그인 실패했습니다')
-
-# def s_logout(request):
-#     next_page = request.GET.get('next')
-#     if request.user.is_authenticated:
-#         # 로그인되어 있는 경우 로그아웃 수행
-#         print("로그아웃 성공적으로 진행함")
-#         logout(request)
-#     if next_page:
-#         # 로그아웃 후에 이전 페이지로 리다이렉션됩니다.
-#         return redirect(next_page)
-#     else:
-#         # 이전 페이지가 없으면 홈페이지로 리다이렉션됩니다.
-#         return redirect('home')  # 이 부분에서 적절한 리다이렉션 대상을 설정해야 합니다.
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from datetime import date
+from datetime import datetime
 
 # Create your views here.
 class CategoryList(ListView):
@@ -73,10 +48,6 @@ def product_detail(request, product_id):
     }
     return render(request, 'customer/product_detail.html', context)
 
-from django.shortcuts import redirect
-from django.contrib.auth import authenticate,login,logout
-from django.http import HttpResponse
-
 def login_view(request):
     if request.method == "POST":
         username = request.POST['username']
@@ -96,23 +67,73 @@ def logout_view(request):
     logout(request)
     return redirect("customer:login")
 
+@login_required
 def quick_checkout(request):
-    user = request.user
-    # 선택한 상품 불러오기
-    product_id = request.GET.get('product_id')
-    product = Product.objects.get(product_id=product_id)
-    # 선택한 상품 수량 불러오기
-    quantity = request.GET.get('quantity')
-    # 가격 계산하기
-    discounted_price = product.price * (1-product.discount_rate)
-    final_price = discounted_price * int(quantity)
-    context = {
-    'user' : user,
-    'product': product,
-    'quantity': quantity,
-    'discounted_price' : discounted_price,
-    'final_price' : final_price
-    }
-    return render(request, 'customer/checkout.html', context)
+    if request.method == 'GET': # 페이지 로딩시
+        # 구매자 정보 불러오기
+        user = request.user
+        # 선택한 상품 불러오기
+        product_id = request.GET.get('product_id')
+        product = Product.objects.get(product_id=product_id)
+        # 선택한 상품 수량 불러오기
+        quantity = request.GET.get('quantity')
+        # 가격 계산하기
+        discounted_price = product.price * (1-product.discount_rate)
+        final_price = discounted_price * int(quantity)
+
+        context = {
+        'user' : user,
+        'product': product,
+        'quantity': quantity,
+        'discounted_price' : discounted_price,
+        'final_price' : final_price
+        }
+        return render(request, 'customer/checkout.html', context)
+    
+    if request.method == 'POST': # 주문하기 버튼이 눌린 경우
+        # 구매자 정보 불러오기
+        user = request.user
+        # 선택한 상품 수량 불러오기
+        quantity = request.POST.get('quantity')
+        # 선택한 상품 불러오기
+        product_id = request.POST.get('product_id')
+        product = Product.objects.get(product_id=product_id)
+        # 가격 계산하기
+        # discounted_price = product.price * (1-product.discount_rate)
+        # final_price = discounted_price * int(quantity)
+        # 배송 정보 불러오기
+        shipping_address = request.POST.get('shipping_address')
+        shipping_address_detail = request.POST.get('shipping_address_detail')
+        postal_code = request.POST.get('postal_code')
+        recipient = request.POST.get('recipient')
+        recipient_phone_number = request.POST.get('recipient_phone_number')
+        payment_method = request.POST.get('payment_method')
+
+        # 현재 날짜 가져오기
+        today = date.today()
+
+        # 주문서 만들기
+        order = Order.objects.create(
+                    # order_id = 3,
+                    customer = user,
+                    order_date = today,
+                    order_status = '주문생성',
+                    shipping_address = shipping_address + ' ' + shipping_address_detail,
+                    postal_code = postal_code,
+                    recipient = recipient,
+                    recipient_phone_number = recipient_phone_number,
+                    payment_method = payment_method
+                )
+        OrderItem.objects.create(
+                    order = order,
+                    product=product,
+                    quantity=quantity
+                )
+        
+        # product 테이블에서 재고 줄여야 함. 결제 완료 후
+        # db에서 말고 동시성처리?
+        return render(request, 'customer/order_confirmation.html')
+
+
 
 
