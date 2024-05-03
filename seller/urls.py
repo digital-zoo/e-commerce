b1e2810fd68bf9091d50dfefb62e51ed2daea60a
goldangeee <goldangeee@proton.me>
2024-05-03 09:02:49 +0900

    판매자 로그인 실패

--- seller/urls.py
+++ seller/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('login/', seller_login_view, name='seller_login'),
     path('logout/', seller_logout_view, name='seller_logout'),
     path('signup/', seller_signup_view, name='seller_signup'),
+    path('mypage/', seller_mypage_view, name='seller_mypage'),    
+    # path('mypage/change_password', seller_change_password_view, name='seller_change_password'),    
+    # path('mypage/profile_edit/', seller_profile_edit_view, name='seller_profile_edit'),
+    # path('mypage/delete_seller/', delete_seller_view, name='delete_seller_view'), 
 ]
--- seller/views.py
+++ seller/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import SellerSignupForm
-from django.contrib.auth import authenticate,login,logout
 from django.http import HttpResponse
-from django.contrib import messages
 from seller.backends import SellerAuthenticationBackend
 
 def seller_login_view(request):
@@ -10,31 +8,32 @@ def seller_login_view(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
 
-        # SellerAuthenticationBackend의 인스턴스를 생성하고 authenticate를 호출합니다.
+        # SellerAuthenticationBackend의 인스턴스를 생성하고 authenticate를 호출
         backend = SellerAuthenticationBackend()
         seller = backend.authenticate(request, username=username, password=password)
         
         if seller:  # 인증 성공
-            # 인증된 사용자를 session에 등록합니다.
-            login(request, seller, backend='seller.backends.SellerAuthenticationBackend')
-            # return redirect('home')
-            return HttpResponse('로그인 성공')
+            #seller 로그인
+            return redirect('seller:seller_mypage')
         else:
             return HttpResponse('로그인 실패. 판매자 계정 정보를 확인해주세요.')
 
     return render(request, 'seller/seller_login.html')
 
 def seller_logout_view(request):
-    logout(request)    
+    #seller 로그아웃   
     return redirect("seller:seller_login")
 
 def seller_signup_view(request):
     if request.method == 'POST':
         form = SellerSignupForm(request.POST)
         if form.is_valid():
-            form.save()
-            # 회원가입 후 로직을 추가하세요. 예: 로그인 페이지로 리다이렉트
+            form.save()            
             return redirect('seller:seller_login')
     else:
         form = SellerSignupForm()
     return render(request, 'seller/seller_signup.html', {'form': form})
+
+#마이페이지 mypage_view
+def seller_mypage_view(request):
+    return render(request,"seller/seller_mypage.html")
